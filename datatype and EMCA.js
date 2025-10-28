74d5b2b89c01eb27b932990d06b6d482334977c1
jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2025-10-28 14:59:09 +0500

    Datatype and ECMA

---
+++ datatype and EMCA.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log("Hello, World!");
+
+let name = "ChatGPT";
+let age = 18;
+let isLoggedIn = true;
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof isLoggedIn);
+console.log(typeof undefined);
