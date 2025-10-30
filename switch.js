0019ee32ae5265f80546e1d6d36473da2ca684c7
jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2025-10-30 11:35:48 +0500

    worked on SWITCH

---
+++ switch.js
@@ -0,0 +1,25 @@
+// switch(key){
+//     case value:
+//         // code block
+//         break;
+//     default:
+//         // code block
+// }
+
+const month = 2
+switch(month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    default:
+        console.log("Default case Run");
+}
