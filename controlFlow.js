55c19486b771051b284503ec19ead99b8aeb928f
jaleed khan <132694164+JaleedKhanofficial@users.noreply.github.com>
2025-10-30 11:20:19 +0500

    Worked on Control Flow

---
+++ controlFlow.js
@@ -0,0 +1,29 @@
+// if (true){
+
+// }
+
+// if (false){
+
+// }
+
+// ==============================================
+// const balance = 100;
+// if (balance < 50){
+//     console.log("Low balance alert!");
+// }
+// else if (balance >= 50 && balance < 200){
+//     console.log("Medium balance.");
+// }
+// else {
+//     console.log("High balance.");
+// }
+
+// ==============================================
+
+const loggedInFromEmail = true
+const loggedInFromGoogle = false
+
+if (loggedInFromEmail || loggedInFromGoogle){
+    console.log("User is logged in");
+    console.log("Welcome back!");
+}
